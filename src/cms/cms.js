684fa3cde92f2faf73bd6f0a7103bb3716a6c07c
Ulisses Ferreira <fc49430@alunos.fc.ul.pt>
2019-03-07 00:54:09 +0000

    chore: remove some more unneeded files

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,10 +1,6 @@
 import CMS from 'netlify-cms'
 
-import AboutPagePreview from './preview-templates/AboutPagePreview'
-import BlogPostPreview from './preview-templates/BlogPostPreview'
-import ProductPagePreview from './preview-templates/ProductPagePreview'
 import IndexPagePreview from './preview-templates/IndexPagePreview'
-
 import DrawingPostPreview from './preview-templates/DrawingPostPreview'
 import GraphicsDesignPostPreview from './preview-templates/GraphicsDesignPostPreview'
 import InteriorDesignPostPreview from './preview-templates/InteriorDesignPostPreview'
@@ -13,10 +9,6 @@ import SculpturePostPreview from './preview-templates/SculpturePostPreview'
 import WritingPostPreview from './preview-templates/WritingPostPreview'
 
 CMS.registerPreviewTemplate('index', IndexPagePreview)
-CMS.registerPreviewTemplate('about', AboutPagePreview)
-CMS.registerPreviewTemplate('products', ProductPagePreview)
-CMS.registerPreviewTemplate('blog', BlogPostPreview)
-
 CMS.registerPreviewTemplate('drawing', DrawingPostPreview)
 CMS.registerPreviewTemplate('graphics-design', GraphicsDesignPostPreview)
 CMS.registerPreviewTemplate('interior-design', InteriorDesignPostPreview)
