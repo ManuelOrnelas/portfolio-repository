76efd202377f42dd9ac662c738b7ab6eaaeff85a
Fábio Ferreira <ffcfpten@gmail.com>
2019-03-12 08:28:12 +0000

    chores: add favicon, icons and logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -36,7 +36,7 @@ const Navbar = class extends React.Component {
         <div className="container">
           <div className="navbar-brand">
             <Link to="/" className="navbar-item" title="Logo">
-              <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
+              <img src={logo} alt="NUCABE" style={{ width: '88px' }} />
             </Link>
             {/* Hamburger menu */}
             <div className="navbar-burger burger" data-target="navMenu">
