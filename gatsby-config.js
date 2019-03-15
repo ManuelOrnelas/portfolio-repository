5000948c3887b2f13e934cbe2bdfa133b0c1a738
Ulisses Ferreira <fc49430@alunos.fc.ul.pt>
2019-03-15 00:20:29 +0000

    feat: update site name

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: 'Nucabe',
     description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+      'Manuel Ornelas\' Portfolio',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
