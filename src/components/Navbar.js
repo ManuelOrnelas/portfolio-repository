722664094d41cc5d80b7c96b16bd9312dec13b4d
Ulisses Ferreira <fc49430@alunos.fc.ul.pt>
2019-03-15 11:47:31 +0000

    feat: navbar first version

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,71 +3,41 @@ import { Link } from 'gatsby'
 import logo from '../img/logo.svg'
 
 const Navbar = class extends React.Component {
-  componentDidMount() {
-    // Get all "navbar-burger" elements
-    const $navbarBurgers = Array.prototype.slice.call(
-      document.querySelectorAll('.navbar-burger'),
-      0
-    )
-    // Check if there are any navbar burgers
-    if ($navbarBurgers.length > 0) {
-      // Add a click event on each of them
-      $navbarBurgers.forEach(el => {
-        el.addEventListener('click', () => {
-          // Get the target from the "data-target" attribute
-          const target = el.dataset.target
-          const $target = document.getElementById(target)
 
-          // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-          el.classList.toggle('is-active')
-          $target.classList.toggle('is-active')
-        })
-      })
+  setActive = (event) => {
+    if (event.target.children.length === 0) { //Carregou diretamente na bolinha
+      event.target.classList.remove('circle')
+      event.target.classList.add('square')
+    } else { //Carregou na div logo a bolinha é o primeiro filho
+      event.target.children[0].remove('circle')
+      event.target.children[0].add('square')
     }
   }
 
   render() {
     return (
-      <nav
-        className="navbar is-transparent"
-        role="navigation"
-        aria-label="main-navigation"
-      >
-        <div className="container">
-          <div className="navbar-brand">
-            <Link to="/" className="navbar-item" title="Logo">
-              <img src={logo} alt="NUCABE" style={{ width: '88px' }} />
-            </Link>
-            {/* Hamburger menu */}
-            <div className="navbar-burger burger" data-target="navMenu">
-              <span />
-              <span />
-              <span />
-            </div>
-          </div>
-          <div id="navMenu" className="navbar-menu">
-            <div className="navbar-start has-text-centered">
-              <Link className="navbar-item" to="/sculpture">
-                Sculpture
-              </Link>
-              <Link className="navbar-item" to="/graphics-design">
-                Graphics Design
-              </Link>
-              <Link className="navbar-item" to="/product-design">
-                Product Design
-              </Link>
-              <Link className="navbar-item" to="/drawing">
-                Drawing
-              </Link>
-              <Link className="navbar-item" to="/writing">
-                Writing
-              </Link>
-              <Link className="navbar-item" to="/interior-design">
-                Interior Design
-              </Link>
-            </div>
-          </div>
-        </div>
+      <nav>
+        <Link to="/" title="Logo">
+          <img src={logo} alt="NUCABE" style={{ width: '60px' }} />
+        </Link>
+        <Link className="flex-center" to="/sculpture" onClick={this.setActive}>
+          <div className="circle"/>
+        </Link>
+        <Link className="flex-center" to="/graphics-design" onClick={this.setActive}>
+          <div className="circle"/>
+        </Link>
+        <Link className="flex-center" to="/product-design" onClick={this.setActive}>
+          <div className="circle"/>
+        </Link>
+        <Link className="flex-center" to="/drawing" onClick={this.setActive}>
+          <div className="circle"/>
+        </Link>
+        <Link className="flex-center" to="/writing" onClick={this.setActive}>
+          <div className="circle"/>
+        </Link>
+        <Link className="flex-center" to="/interior-design" onClick={this.setActive}>
+          <div className="circle"/>
+        </Link>
       </nav>
     )
   }
