305fb1667d1d38610b5a8dd914cf2a4a7289520d
Ulisses Ferreira <fc49430@alunos.fc.ul.pt>
2019-03-15 16:05:42 +0000

    feat: navbar finally working

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,89 +4,29 @@ import logo from '../img/logo.svg'
 
 const Navbar = class extends React.Component {
 
-  setActiveSculpture = (event) => {
-    console.log('batata')
-    let el = document.getElementById('sculpture')
-    console.log(el)
-    el.classList.add('square')
-    console.log(el.classList)
-    el.classList.remove('circle')
-    console.log(el.classList)
-  }
-
-  setActiveGraphicsDesign = (event) => {
-    console.log('batata')
-    let el = document.getElementById('graphics-design')
-    console.log(el)
-    el.classList.add('square')
-    console.log(el.classList)
-    el.classList.remove('circle')
-    console.log(el.classList)
-  }
-
-  setActiveProductDesign = (event) => {
-    console.log('batata')
-    let el = document.getElementById('product-design')
-    console.log(el)
-    el.classList.add('square')
-    console.log(el.classList)
-    el.classList.remove('circle')
-    console.log(el.classList)
-  }
-
-  setActiveDrawing = (event) => {
-    console.log('batata')
-    let el = document.getElementById('drawing')
-    console.log(el)
-    el.classList.add('square')
-    console.log(el.classList)
-    el.classList.remove('circle')
-    console.log(el.classList)
-  }
-
-  setActiveWriting = (event) => {
-    console.log('batata')
-    let el = document.getElementById('writing')
-    console.log(el)
-    el.classList.add('square')
-    console.log(el.classList)
-    el.classList.remove('circle')
-    console.log(el.classList)
-  }
-
-  setActiveInteriorDesign = (event) => {
-    console.log('batata')
-    let el = document.getElementById('interior-design')
-    console.log(el)
-    el.classList.add('square')
-    console.log(el.classList)
-    el.classList.remove('circle')
-    console.log(el.classList)
-  }
-
   render() {
     return (
       <nav id="navbar">
         <Link to="/" title="Logo">
           <img src={logo} alt="NUCABE" style={{ width: '60px' }} />
         </Link>
-        <Link className="flex-center" to="/sculpture" onClick={this.setActiveSculpture}>
-          <div id="sculpture" className="circle"/>
+        <Link className="flex-center" to="/sculpture" data-target="sculpture">
+          <div id="sculpture" className={window.location.pathname==='/sculpture' ? 'square' : 'circle'}/>
         </Link>
-        <Link className="flex-center" to="/graphics-design" onClick={this.setActiveGraphicsDesign}>
-          <div id="graphics-design" className="circle"/>
+        <Link className="flex-center" to="/graphics-design" data-target="graphics-design">
+          <div id="graphics-design" className={window.location.pathname==='/graphics-design' ? 'square' : 'circle'}/>
         </Link>
-        <Link className="flex-center" to="/product-design" onClick={this.setActiveProductDesign}>
-          <div id="product-design" className="circle"/>
+        <Link className="flex-center" to="/product-design" data-target="product-design">
+          <div id="product-design" className={window.location.pathname==='/product-design' ? 'square' : 'circle'}/>
         </Link>
-        <Link className="flex-center" to="/drawing" onClick={this.setActiveDrawing}>
-          <div id="drawing" className="circle"/>
+        <Link className="flex-center" to="/drawing" data-target="drawing">
+          <div id="drawing" className={window.location.pathname==='/drawing' ? 'square' : 'circle'}/>
         </Link>
-        <Link className="flex-center" to="/writing" onClick={this.setActiveWriting}>
-          <div id="writing" className="circle"/>
+        <Link className="flex-center" to="/writing" data-target="writing">
+          <div id="writing" className={window.location.pathname==='/writing' ? 'square' : 'circle'}/>
         </Link>
-        <Link className="flex-center" to="/interior-design" onClick={this.setActiveInteriorDesign}>
-          <div id="interior-design" className="circle"/>
+        <Link className="flex-center" to="/interior-design" data-target="interior-design">
+          <div id="interior-design" className={window.location.pathname==='/interior-design' ? 'square' : 'circle'}/>
         </Link>
       </nav>
     )
