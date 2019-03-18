f6f89c4b21f0d871ebd57e73f368e64a07c8213f
Fábio Ferreira <ffcfpten@gmail.com>
2019-03-18 18:04:51 +0000

    chores: change to SCSS syntax

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -5,7 +5,7 @@ import { StaticQuery, graphql } from 'gatsby'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
 
-import './all.sass'
+import './all.scss'
 
 const TemplateWrapper = ({ children }) => (
   <StaticQuery
