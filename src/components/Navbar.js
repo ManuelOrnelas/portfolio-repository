21050c17721a9fdb8b550242efedd9319df344a5
Fábio Ferreira <ffcfpten@gmail.com>
2019-03-19 16:27:57 +0000

    fix: navbar window not defined

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,10 +2,11 @@ import React from 'react'
 import { Link } from 'gatsby'
 import logo from '../img/logo.svg'
 
-const Navbar = class extends React.Component {
-
+export default class Navbar extends React.Component {
   render() {
-    // console.log(window.location.pathname)
+    const windowGlobal = typeof window !== 'undefined' && window;
+    if(windowGlobal) console.log(windowGlobal.location.pathname)
+
     return (
       <nav id="navbar" style={{backgroundColor: this.props.color}}>
         <Link to="/" title="Logo" className='logo'>
@@ -13,27 +14,43 @@ const Navbar = class extends React.Component {
         </Link>
         <div id='item-list' className='flex'>
           <Link className="flex justifycontent-center alignitems-center" to="/sculpture" data-target="sculpture">
-            <div id="sculpture" className={window.location.pathname==='/sculpture' ? 'square' : 'circle'}/>
+            <div id="sculpture" className={windowGlobal ?
+              (windowGlobal.location.pathname==='/sculpture' ? 'square' : 'circle')
+              : null
+            }/>
           </Link>
           <Link className="flex justifycontent-center alignitems-center" to="/graphics-design" data-target="graphics-design">
-            <div id="graphics-design" className={window.location.pathname==='/graphics-design' ? 'square' : 'circle'}/>
+            <div id="graphics-design" className={windowGlobal ?
+              (windowGlobal.location.pathname==='/graphics-design' ? 'square' : 'circle')
+              : null
+            }/>
           </Link>
           <Link className="flex justifycontent-center alignitems-center" to="/product-design" data-target="product-design">
-            <div id="product-design" className={window.location.pathname==='/product-design' ? 'square' : 'circle'}/>
+            <div id="product-design" className={windowGlobal ?
+              (windowGlobal.location.pathname==='/product-design' ? 'square' : 'circle')
+              : null
+            }/>
           </Link>
           <Link className="flex justifycontent-center alignitems-center" to="/drawing" data-target="drawing">
-            <div id="drawing" className={window.location.pathname==='/drawing' ? 'square' : 'circle'}/>
+            <div id="drawing" className={windowGlobal ?
+              (windowGlobal.location.pathname==='/drawing' ? 'square' : 'circle')
+              : null
+            }/>
           </Link>
           <Link className="flex justifycontent-center alignitems-center" to="/writing" data-target="writing">
-            <div id="writing" className={window.location.pathname==='/writing' ? 'square' : 'circle'}/>
+            <div id="writing" className={windowGlobal ? 
+              (windowGlobal.location.pathname==='/writing' ? 'square' : 'circle')
+              : null
+            }/>
           </Link>
           <Link className="flex justifycontent-center alignitems-center" to="/interior-design" data-target="interior-design">
-            <div id="interior-design" className={window.location.pathname==='/interior-design' ? 'square' : 'circle'}/>
+            <div id="interior-design" className={windowGlobal ? 
+              (windowGlobal.location.pathname==='/interior-design' ? 'square' : 'circle')
+              : null
+            }/>
           </Link>
         </div>
       </nav>
     )
   }
 }
-
-export default Navbar
