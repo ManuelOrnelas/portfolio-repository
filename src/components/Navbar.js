726c2ce4c4e627403f013a2eb3367d2cd91b2912
Fábio Ferreira <ffcfpten@gmail.com>
2019-03-20 01:05:15 +0000

    fix: navbar problem

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,14 +3,28 @@ import { Link } from 'gatsby'
 import logo from '../img/logo.svg'
 
 export default class Navbar extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      path: '',
+    }
+  }
+
+  componentDidMount() {
+    this.setState({
+      path: window.location.pathname
+    })
+  }
+
   render() {
     // fetch color
     // let color = '';
     // if (this.props.color) color = this.props.color.replace('\\','')
     
     // define window
-    const windowGlobal = typeof window !== 'undefined' && window;
-    if(windowGlobal) console.log(windowGlobal.location.pathname)
+    // const windowGlobal = typeof window !== 'undefined' && window;
+    // if(windowGlobal) console.log(windowGlobal.location.pathname)
 
     return (
       <nav id="navbar" style={{backgroundColor: this.props.color }}>
@@ -19,44 +33,44 @@ export default class Navbar extends React.Component {
         </Link>
         <div id='item-list' className='flex'>
           <Link className="flex justifycontent-center alignitems-center" to="/" data-target="root">
-            <div id="root" className={windowGlobal ?
-              (windowGlobal.location.pathname==='/' ? 'square' : 'circle')
+            <div id="root" className={this.state.path ?
+              (this.state.path === '/' ? 'square' : 'circle')
               : null
             }/>
           </Link>
           <Link className="flex justifycontent-center alignitems-center" to="/sculpture" data-target="sculpture">
-            <div id="sculpture" className={windowGlobal ?
-              (windowGlobal.location.pathname==='/sculpture' ? 'square' : 'circle')
+            <div id="sculpture" className={this.state.path ?
+              (this.state.path  === '/sculpture' ? 'square' : 'circle')
               : null
             }/>
           </Link>
           <Link className="flex justifycontent-center alignitems-center" to="/graphics-design" data-target="graphics-design">
-            <div id="graphics-design" className={windowGlobal ?
-              (windowGlobal.location.pathname==='/graphics-design' ? 'square' : 'circle')
+            <div id="graphics-design" className={this.state.path ?
+              (this.state.path  === '/graphics-design' ? 'square' : 'circle')
               : null
             }/>
           </Link>
           <Link className="flex justifycontent-center alignitems-center" to="/product-design" data-target="product-design">
-            <div id="product-design" className={windowGlobal ?
-              (windowGlobal.location.pathname==='/product-design' ? 'square' : 'circle')
+            <div id="product-design" className={this.state.path ?
+              (this.state.path === '/product-design' ? 'square' : 'circle')
               : null
             }/>
           </Link>
           <Link className="flex justifycontent-center alignitems-center" to="/drawing" data-target="drawing">
-            <div id="drawing" className={windowGlobal ?
-              (windowGlobal.location.pathname==='/drawing' ? 'square' : 'circle')
+            <div id="drawing" className={this.state.path ?
+              (this.state.path === '/drawing' ? 'square' : 'circle')
               : null
             }/>
           </Link>
           <Link className="flex justifycontent-center alignitems-center" to="/writing" data-target="writing">
-            <div id="writing" className={windowGlobal ? 
-              (windowGlobal.location.pathname==='/writing' ? 'square' : 'circle')
+            <div id="writing" className={this.state.path  ? 
+              (this.state.path === '/writing' ? 'square' : 'circle')
               : null
             }/>
           </Link>
           <Link className="flex justifycontent-center alignitems-center" to="/interior-design" data-target="interior-design">
-            <div id="interior-design" className={windowGlobal ? 
-              (windowGlobal.location.pathname==='/interior-design' ? 'square' : 'circle')
+            <div id="interior-design" className={this.state.path  ? 
+              (this.state.path === '/interior-design' ? 'square' : 'circle')
               : null
             }/>
           </Link>
