1b5d71f9692d76c27d26f5a92575eef2e1a6992c
Fábio Ferreira <ffcfpten@gmail.com>
2019-03-20 23:09:27 +0000

    styles: mobile responsiveness

--- src/pages/drawing/index.js
+++ src/pages/drawing/index.js
@@ -10,7 +10,7 @@ const DrawingPage = ({data}) => {
   return (
     <Layout primaryColor={color}>
       <div className='full-page flex justifycontent-center alignitems-center bcg yellow white-text'>
-        <h1 id='page-title' className='huge-text'>Drawing.</h1>
+        <h1 id='page-title' className='huge-text text-center'>Drawing.</h1>
       </div>
       <div className='full-page'>
         <div className='lateral-space'>
