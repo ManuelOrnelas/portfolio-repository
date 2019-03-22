11cfb8cea5b72131561e6803c4a11fa76b05ab44
Fábio Ferreira <ffcfpten@gmail.com>
2019-03-22 01:06:03 +0000

    fix: navbar not working in posts

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -18,6 +18,15 @@ export default class Navbar extends React.Component {
   }
 
   render() {
+    let urlParts = this.state.path.split('/')
+
+    // get setcion from URL pathname
+    let section = ''
+
+    // if the URL parts array is bigger than 1
+    // then it means the URL is different than "/"
+    if(urlParts.length > 1) section = urlParts[1]
+
     return (
       <nav id="navbar" style={{backgroundColor: this.props.color }}>
         <Link to="/" title="Logo" className='logo flex aligncontent-center'>
@@ -31,37 +40,37 @@ export default class Navbar extends React.Component {
             }/>
           </Link>
           <Link className="flex justifycontent-center alignitems-center" to="/sculpture" data-target="sculpture">
-            <div id="sculpture" className={this.state.path  === '/sculpture'
+            <div id="sculpture" className={section.includes('sculpture')
               ? 'square'
               : 'circle'
             }/>
           </Link>
           <Link className="flex justifycontent-center alignitems-center" to="/graphics-design" data-target="graphics-design">
-            <div id="graphics-design" className={this.state.path  === '/graphics-design'
+            <div id="graphics-design" className={section.includes('graphics-design')
               ? 'square'
               : 'circle'
             }/>
           </Link>
           <Link className="flex justifycontent-center alignitems-center" to="/product-design" data-target="product-design">
-            <div id="product-design" className={this.state.path === '/product-design'
+            <div id="product-design" className={section.includes('product-design')
               ? 'square'
               : 'circle'
             }/>
           </Link>
           <Link className="flex justifycontent-center alignitems-center" to="/drawing" data-target="drawing">
-            <div id="drawing" className={this.state.path === '/drawing'
+            <div id="drawing" className={section.includes('drawing')
               ? 'square'
               : 'circle'
             }/>
           </Link>
           <Link className="flex justifycontent-center alignitems-center" to="/writing" data-target="writing">
-            <div id="writing" className={this.state.path === '/writing'
+            <div id="writing" className={section.includes('writing')
               ? 'square'
               : 'circle'
             }/>
           </Link>
           <Link className="flex justifycontent-center alignitems-center" to="/interior-design" data-target="interior-design">
-            <div id="interior-design" className={this.state.path === '/interior-design'
+            <div id="interior-design" className={section.includes('interior-design')
               ? 'square'
               : 'circle'
             }/>
