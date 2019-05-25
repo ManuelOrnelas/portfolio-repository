88763e9a3ea522c7340da73b7edc443edddd6c2a
Ulisses Ferreira <ulissesvf@gmail.com>
2019-05-25 14:45:08 +0100

    chore: replace metadata information with real values

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,9 +4,9 @@ require('dotenv').config({
 
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Nucabe`,
+    description: `I am Manuel Ornelas`,
+    author: `@fabiofcferreira @ulissesvf`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
