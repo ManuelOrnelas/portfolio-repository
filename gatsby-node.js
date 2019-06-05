827292312cea66dfb986ebb26e48ad4068864d0c
Ulisses Ferreira <ulissesvf@gmail.com>
2019-06-06 00:58:33 +0100

    revert: last commits. problem found

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,13 +1,5 @@
 const path = require('path')
 
-exports.onCreateWebpackConfig = ({ stage, actions }) => {
-  actions.setWebpackConfig({
-    resolve: {
-      modules: [path.resolve(__dirname, "src"), "node_modules"],
-    },
-  })
-}
-
 exports.createPages = ({graphql, actions}) => {
 
   const {createPage} = actions
