72300a84763892a3ce0adcac4f45a5dd401a7ad1
Ulisses Ferreira <ulissesvf@gmail.com>
2019-06-06 19:02:24 +0100

    wip: first version of working sidebar (may be optimized)

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 
 /*
   Creates a sidebar navigation with 3 links.
@@ -7,8 +7,40 @@ import React, { useState } from "react"
 const Sidebar = (props) => {
 
   const [index, setIndex] = useState(0);
+  const remToPx = (typeof window !== `undefined`) ? parseFloat(getComputedStyle(document.documentElement).fontSize) : 0
 
-  if (props.hide) {
+  useEffect(() => {
+
+    //ComponentDidMount
+    window.addEventListener('scroll', stickyMode)
+
+    return function cleanup() {
+      //ComponentWillUnmount
+      window.removeEventListener('scroll', stickyMode)
+    }
+  }, [])
+
+  const stickyMode = () => {
+    if (typeof window !== `undefined`) {
+      const currentScroll = window.pageYOffset + 3.5 * remToPx
+      // Now we get the viewports height.
+      const maxHeight = window.innerHeight
+
+      const page = ~~(currentScroll / maxHeight)
+
+      if (page === 0) {
+        setIndex(0)
+      } else if (page === 1) {
+        setIndex(1)
+      } else if (page === 2) {
+        setIndex(2)
+      } else if (page === 3) {
+        setIndex(3)
+      }
+    }
+  }
+
+  if (index === 0) {
     return (
       <>
       </>
